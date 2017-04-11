bd5f26f666e55a812a50dbd55451f5a983413dd6
7daysdreamfornew <7daysdreamfornew@gmail.com>
2017-04-12 09:00:41 +1200

    Updated ProductOptionsServiceTests to get newly created product

--- refactor-me.test/ProductOptionsServiceTests.cs
+++ refactor-me.test/ProductOptionsServiceTests.cs
@@ -10,37 +10,17 @@ namespace refactor_me.test
     public class ProductOptionsServiceTests
     {
         [TestMethod]
-        public void Test7_GetProductOptionByIdTest()
-        {
-            // iphone gold
-            ProductOptionsService productOptionsService = new ProductOptionsService();
-            ProductOption output = productOptionsService.GetProductOptionById(new Guid("5c2996ab-54ad-4999-92d2-89245682d534"));
-            Assert.IsNotNull(output);
-            Assert.AreEqual(new Guid("5c2996ab-54ad-4999-92d2-89245682d534"), output.Id);
-        }
-
-        [TestMethod]
-        public void Test8_GetProductOptionsByProductIdTest()
-        {            
-            ProductOptionsService productOptionsService = new ProductOptionsService();
-            // iPhone options to find
-            ProductOptions output = productOptionsService.GetProductOptionsByProductId(new Guid("de1287c0-4b15-4a7b-9d8a-dd21b3cafec3"));
-            Assert.IsNotNull(output);
-            Assert.AreNotEqual(0, output.Items.Count);
-        }
-        
-        [TestMethod]
         public void Test9_CreateProductOptionTest()
         {
             ProductsService productsService = new ProductsService();
-            Products createdDetails = productsService.GetProductsByName("Samsung Galaxy S7");
+            Products createdDetails = productsService.GetProductsByName("Sony Xperia X Compact");
 
             ProductOptionsService productOptionsService = new ProductOptionsService();
             ProductOption input = new ProductOption
             {
                 ProductId = createdDetails.Items[0].Id,
-                Name = "Rose Gold",
-                Description = "Rose Gold Samsung"
+                Name = "White",
+                Description = "White Sony"
             };
 
             productOptionsService.CreateOption(input);
@@ -54,7 +34,7 @@ namespace refactor_me.test
         public void Test10_UpdateProductOptionTest()
         {
             ProductsService productsService = new ProductsService();
-            Products createdProductDetails = productsService.GetProductsByName("Samsung Galaxy S7");
+            Products createdProductDetails = productsService.GetProductsByName("Sony Xperia X Compact");
 
             ProductOptionsService productOptionsService = new ProductOptionsService();
             ProductOptions createdProductOptionDetails = productOptionsService.GetProductOptionsByProductId(createdProductDetails.Items[0].Id);
@@ -65,7 +45,7 @@ namespace refactor_me.test
                 Id = createdProductOptionDetails.Items[0].Id,
                 ProductId = createdProductOptionDetails.Items[0].ProductId,
                 Name = "Red",
-                Description = "Red Samsung"
+                Description = "Red Sony"
             };
 
             productOptionsService.UpdateOption(input);
@@ -75,12 +55,37 @@ namespace refactor_me.test
             // Should fail the test here
             Assert.AreEqual("White", output.Name);
         }
+        [TestMethod]
+        public void Test7_GetProductOptionByIdTest()
+        {
+            ProductsService productsService = new ProductsService();
+            Products createdDetails = productsService.GetProductsByName("Sony Xperia X Compact");
+            ProductOptionsService productOptionsService = new ProductOptionsService();
+
+            ProductOption output = productOptionsService.GetProductOptionById(createdDetails.Items[0].Id);
+            Assert.IsNotNull(output);
+            Assert.AreEqual(new Guid("5c2996ab-54ad-4999-92d2-89245682d534"), output.Id);
+        }
+
+        [TestMethod]
+        public void Test8_GetProductOptionsByProductIdTest()
+        {
+            ProductsService productsService = new ProductsService();
+            Products createdDetails = productsService.GetProductsByName("Sony Xperia X Compact");
+            ProductOptionsService productOptionsService = new ProductOptionsService();
+        
+            ProductOptions output = productOptionsService.GetProductOptionsByProductId(createdDetails.Items[0].Id);
+            Assert.IsNotNull(output);
+            Assert.AreNotEqual(0, output.Items.Count);
+        }
+        
+     
 
         [TestMethod]
         public void Test11_DeleteProdutOptionTest()
         {
             ProductsService productsService = new ProductsService();
-            Products createdProductDetails = productsService.GetProductsByName("Samsung Galaxy S7");
+            Products createdProductDetails = productsService.GetProductsByName("Sony Xperia X Compact");
 
             ProductOptionsService productOptionsService = new ProductOptionsService();
             ProductOptions createdProductOptionDetails = productOptionsService.GetProductOptionsByProductId(createdProductDetails.Items[0].Id);
